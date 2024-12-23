977815b16b13ed719218bf8fa733fe319075f597
Bryce Monaco <brycedmonaco@gmail.com>
2024-12-22 17:21:59 -0800

    Completed "I Am Poor" challenge project

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Bryce Monaco on 12/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
